011ea01f14988aaf967fb2c0dea53edfffd63192
Morris Liu <morrisliu1994@outlook.com>
2026-02-28 21:50:26 +0800

    fix: avoid compile-time dotenv macros in examples

--- examples/get_api_key.rs
+++ examples/get_api_key.rs
@@ -1,9 +1,9 @@
-use dotenvy_macro::dotenv;
 use openrouter_rs::OpenRouterClient;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let provisioning_key = dotenv!("OPENROUTER_PROVISIONING_KEY");
+    let provisioning_key = std::env::var("OPENROUTER_PROVISIONING_KEY")
+        .expect("OPENROUTER_PROVISIONING_KEY must be set");
     let client = OpenRouterClient::builder()
         .provisioning_key(provisioning_key)
         .build()?;
--- examples/stream_chat_completion.rs
+++ examples/stream_chat_completion.rs
@@ -1,4 +1,3 @@
-use dotenvy_macro::dotenv;
 use futures_util::StreamExt;
 use openrouter_rs::{
     OpenRouterClient,
@@ -8,7 +7,7 @@ use openrouter_rs::{
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let api_key = dotenv!("OPENROUTER_API_KEY");
+    let api_key = std::env::var("OPENROUTER_API_KEY").expect("OPENROUTER_API_KEY must be set");
     let client = OpenRouterClient::builder().api_key(api_key).build()?;
 
     let chat_request = ChatCompletionRequest::builder()
