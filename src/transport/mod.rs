1576b578cf82d9195aa7fb3fd806cb3b8a216bfe
Morris Liu <morrisliu1994@outlook.com>
2026-04-18 17:38:19 +0800

    chore: format transport client builder

--- src/transport/mod.rs
+++ src/transport/mod.rs
@@ -6,5 +6,7 @@ use reqwest::Client;
 use crate::error::OpenRouterError;
 
 pub(crate) fn new_client() -> Result<Client, OpenRouterError> {
-    reqwest::Client::builder().build().map_err(OpenRouterError::from)
+    reqwest::Client::builder()
+        .build()
+        .map_err(OpenRouterError::from)
 }
